effe8814178325947f49f1a8deedee11e399a5cc
Abdallah Abu Samaha <abdallah.abusamaha@gmail.com>
2022-11-03 14:37:01 -0700

    Initial Commit

---
+++ FollowMeApp/FollowMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowMeAppApp.swift
+//  FollowMeApp
+//
+//  Created by Abdallah Abu Samaha on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FollowMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
